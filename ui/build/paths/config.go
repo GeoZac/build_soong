6b3274ea73dd5890200e51098c1b804261fcc679
Julian Veit <claymore1298@gmail.com>
2020-10-08 23:22:33 +0300

    soong: whitelist repo
    Required for changelog generation
    
    Change-Id: Ica31e9595e8ab722daac7f5b20f0b500cd7090d1

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -113,6 +113,7 @@ var Configuration = map[string]PathConfig{
 	"shuf":           Allowed,
 	"mogrify":        Allowed,
 	"xmllint":        Allowed,
+	"repo":           Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
